3d3ed0061d004cf7e75e83d3108f68534da7207c
jimmyZip <34062954+jimmyZip@users.noreply.github.com>
2019-04-26 12:56:55 +0900

    ㄴㄴ

--- src/com/books/controller/member/MypageController.java
+++ src/com/books/controller/member/MypageController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,23 +15,27 @@ import com.books.model.domain.member.Member;
 import com.books.model.service.member.BookmarkService;
 import com.books.model.service.member.MemberService;
 
-@Controller
-public class MypageController {
-	@Autowired
+
+ @Controller 
+ public class MypageController {
+	 
+	
+ 	@Autowired
 	private BookmarkService bookmarkService;
-	@Autowired
-	private MemberService memberService;
-	
-	@RequestMapping(value="/member/mypage",method=RequestMethod.GET)
-	public ModelAndView markAll(HttpServletRequest request) {
-		Member member = (Member) request.getAttribute("member");
-		List markList=bookmarkService.selectByMember(member.getMember_id());
-		ModelAndView mav = new ModelAndView();
-		mav.setViewName("member/mypage");
-		mav.addObject("markList", markList);
-		return mav;
+ 
+ 	@Autowired 
+ 	private MemberService memberService;
+ 
+ 	@RequestMapping(value="/member/mypage",method=RequestMethod.GET)
+ 	public ModelAndView markAll(HttpServletRequest request) { 
+ 		Member member = (Member)request.getAttribute("member"); 
+ 		List markList=bookmarkService.selectByMember(member.getMember_id()); 
+ 		ModelAndView mav = new ModelAndView(); 
+ 		mav.setViewName("member/mypage");
+ 		mav.addObject("markList", markList); 
+ 		return mav; 
 	}
-	
+ 	
 }
 
 

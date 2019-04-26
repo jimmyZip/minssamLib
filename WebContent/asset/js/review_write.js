8def22615cf949a6661d5a405f6557a446c79afd
jimmyZip <34062954+jimmyZip@users.noreply.github.com>
2019-04-26 12:56:04 +0900

    pull이 안돼서 걍

--- WebContent/asset/js/review_write.js
+++ WebContent/asset/js/review_write.js
@@ -2,15 +2,22 @@
  * 
  */
 $(document).ready(function(){
-	init();	
+	init();
 });
 function init(){
+	//editor 사용
 	CKEDITOR.replace('content');
 	$("input[type='button']").click(function(){
 		send();
 	});
+	
+	//리뷰어가 책에 대해 매긴 평점 카운트
+	$($(".starImg").find("img")).click(function(){
+		scoreCount();
+	});
 }
 
+//form 전송
 function send(){
 	var form=$("form");
 	form.attr({
@@ -18,4 +25,11 @@ function send(){
 		"action":"/reviews"
 	});
 	form.submit();
+}
+
+//책의 평점 계산
+function scoreCount(){
+	alert(11);
+	//몇 번째 별인지 계산
+	
 }

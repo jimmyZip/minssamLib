8587127f6cb930020a2ee30710acb5d7bcc94a0e
syk5095 <40268006+syk5095@users.noreply.github.com>
2019-05-02 16:47:22 +0900

    로그인창 바이러스 제거

--- WebContent/asset/js/login-modal.js
+++ WebContent/asset/js/login-modal.js
@@ -1,14 +1,14 @@
 /*modal popup related with login*/
 $(function(){
-	$("#full,#view").hide();
-	$("#login-bt").click(function(){
-		loginModalShow();
-	});
+   $("#full,#view").hide();
+   $("#login-bt").click(function(){
+      loginModalShow();
+   });
 });
 function loginModalShow(){
-	$("#view").load("/member/login/loginform.jsp");
-	$("#view,#full").show();
-	$("#full").click(function(){		
-		$("#full,#view").hide();
-	});
+   $("#view").load("/member/login/loginform.jsp");
+   $("#view,#full").show();
+   $("#full").click(function(){      
+      $("#full,#view").hide();
+   });
 }
